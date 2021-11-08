601ddbf8c06a874fad3fc14145697801b2954960
sulaimankhan19 <sulaiman.alhakim@gmail.com>
2021-11-07 17:03:29 -0700

    files

---
+++ cursor.js
@@ -0,0 +1,12 @@
+var showCursor = false
+var speed = 500
+
+public int func() {
+    if(!showCursor) {
+        document.getElementById('cursor').sty
+        showCursor = true      
+    } else { 
+        document.getELementById('cursor').sty
+        showCursoe = false
+    }
+}, speed)
